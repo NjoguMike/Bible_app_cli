c65ce6ddc0f6c811d39eb4850f71e0de2fd80629
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-11-01 11:27:13 +0300

    Defined Classes

--- models.py
+++ models.py
@@ -17,3 +17,25 @@ class User(Base):
     def __repr__(self):
         username = self.username
         return username
+    
+class Bible(Base):
+    __tablename__ = 'bibles'
+
+    id = Column(Integer(), primary_key=True)
+    bible_name = Column(String())
+    bible_version = Column(String())
+
+    def __repr__(self):
+        bible = self.bible_name
+        return bible
+
+class Note(Base):
+    __tablename__ = 'notes'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    reference = Column(String())
+
+    def __repr__(self):
+        note = self.title
+        return note
