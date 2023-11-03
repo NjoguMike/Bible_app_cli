9f67e41dba64802c47794cbddf3868dfe1d1ca00
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-11-04 00:43:06 +0300

    App logic and design done

--- bible_app.py
+++ bible_app.py
@@ -6,8 +6,7 @@ from seed import bible_books, bible_version
 
 app_user = User
 note_pad = Note
-bible_ref = Bible
-user_logged = []
+user_session = {}
 
 def add_bible_data():
     session.query(Bible).delete()
@@ -22,29 +21,41 @@ def read_bible():
     select_book = input("Please choose a book: ")
     select_chapter = int(input("and chapter: "))
     select_verse = int(input("and verse (optional): "))
+
+    user_session.update(bible_ref = select_book)
+
     Bible.read_book(select_book,select_chapter,select_verse)
-    # return content
+
 
 def user_registration():
     user = app_user(username = input("Your name please: "),
                  email = input("Email: "))
-    user_logged.append(user)
     session.add(user)
     session.commit()
+    user_session.update(user_logged = user)
+
     print(f"Welcome ! {user.username} \n")
     
 def return_user():
     user_login = input("Please enter your email to sign in: ")
     result = session.query(User).filter(User.email == user_login).first()
-    user_logged.append(result)
+
+    user_session.update(user_logged = result)
+
     print(f"Welcome back ! {result.username}")
 
 def make_notes():
+    result = session.query(Bible).filter(Bible.book == user_session['bible_ref']).first()
+
     new_note = note_pad(title = str(input("Please write the title of your note: ")),
-                        reference = input("What is your preferred Bible Version ? .i.e (LST, ESV, RSV, NRSV, NIV, KJV, NKJV ): "),
-                        user_id = user_logged[0].id)
+                        reference_chapter = user_session['bible_ref'],
+                        bible_version = input("What is your preferred Bible Version ? .i.e (LST, ESV, RSV, NRSV, NIV, JV, NKJV ): "),
+                        content = str(input("Start making notes here : ")),
+                        user_id = user_session['user_logged'].id,
+                        bible_id = result.id)
     session.add(new_note)
     session.commit()
+
     
 
 def app():
@@ -64,18 +75,13 @@ def app():
 
             if ans == 'y':
                 user_registration()
-
                 Bible.books()
                 read_bible()
 
             else:
                 return_user()
-                
                 Bible.books()
                 read_bible()
-
-
-            choice = 3
             
         elif choice == 2:
             ans = input("Are you new? y/n: ")
@@ -85,12 +91,17 @@ def app():
 
                 print("Creating an account ...")
                 time.sleep(3)
+
+                Bible.books()
+                read_bible()
                 make_notes()
 
             else:
                 return_user()
-
                 time.sleep(2)
+
+                Bible.books()
+                read_bible()
                 make_notes()
 
             choice = 3
@@ -99,8 +110,10 @@ def app():
 
 
 if __name__ == '__main__':
-    add_bible_data()
+    # add_bible_data()
     app()
+    # read_bible()
+    # make_notes()
 
 
     
